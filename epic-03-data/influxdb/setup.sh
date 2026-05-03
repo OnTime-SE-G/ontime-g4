aa443ea6be840a21ad353b98dd8434c3df4753ee
teshaaan <teshanperera03@gmail.com>
2026-05-03 20:07:43 +0530

    fix(ci): remove BOM and quote variable in setup.sh to pass shellcheck

--- epic-03-data/influxdb/setup.sh
+++ epic-03-data/influxdb/setup.sh
@@ -1,9 +1,4 @@
-﻿#!/bin/bash
+#!/bin/bash
 set -e
-
-# Wait for InfluxDB to fully boot up
-sleep 5 
-
-echo "Creating gps-aggregates bucket..."
-# Create the 90-day (2160h) retention bucket using the admin token
-influx bucket create -n gps-aggregates -o transit-org -r 2160h -t $DOCKER_INFLUXDB_INIT_ADMIN_TOKEN
+sleep 5
+influx bucket create -n gps-aggregates -o transit-org -r 2160h -t "$DOCKER_INFLUXDB_INIT_ADMIN_TOKEN"
