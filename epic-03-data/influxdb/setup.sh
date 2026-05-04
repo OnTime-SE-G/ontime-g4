9084ba2ecd3a57812fc2fb0aa7f6684806e12675
teshaaan <teshanperera03@gmail.com>
2026-05-04 19:14:25 +0530

    made files combining the G2 repo with G4 issues

--- epic-03-data/influxdb/setup.sh
+++ epic-03-data/influxdb/setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
-sleep 5
-influx bucket create -n gps-aggregates -o transit-org -r 2160h -t "$DOCKER_INFLUXDB_INIT_ADMIN_TOKEN"
+
+influx bucket create \
+  --name gps-aggregates \
+  --org "${DOCKER_INFLUXDB_INIT_ORG:-ontime}" \
+  --retention 2160h \
+  --token "$DOCKER_INFLUXDB_INIT_ADMIN_TOKEN" || true
