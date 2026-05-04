4d00605aa97a01c95a4cb7345bf34d0d5ca7d33a
teshaaan <teshanperera03@gmail.com>
2026-05-04 13:27:19 +0530

    added statefulsets services and postgis ectensions

--- epic-03-data/postgres/init/00-extensions.sql
+++ epic-03-data/postgres/init/00-extensions.sql
@@ -1,11 +1,33 @@
-﻿-- TODO: Implement according to G4 guide and issue assignment.
+SELECT 'CREATE DATABASE keycloak_db ENCODING ''UTF8'' TEMPLATE template0'
+WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'keycloak_db')\gexec
 
--- Create the additional Keycloak database
-CREATE DATABASE keycloak_db;
+SELECT 'CREATE DATABASE ontime_db ENCODING ''UTF8'' TEMPLATE template0'
+WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'ontime_db')\gexec
 
--- Connect to the main transit database (created automatically by the container env vars)
-\c transit_db
+SELECT 'CREATE DATABASE fleet_db ENCODING ''UTF8'' TEMPLATE template0'
+WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'fleet_db')\gexec
 
--- Install the spatial extensions for geo-fencing and route mapping
+SELECT 'CREATE DATABASE ontime_test_db ENCODING ''UTF8'' TEMPLATE template0'
+WHERE NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'ontime_test_db')\gexec
+
+\connect transit_db
+
 CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE EXTENSION IF NOT EXISTS postgis_topology;
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+\connect ontime_db
+
+CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS postgis_topology;
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+\connect fleet_db
+
+CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+\connect ontime_test_db
+
+CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
